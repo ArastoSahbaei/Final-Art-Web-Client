5d92f0ea5fcd6d5603a4be87cd00d0e59d908ecb
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-12-08 15:53:57 +0100

    m

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,6 +1,10 @@
 export const Application = () => {
 	return (
 		<div>
+			1. create board
+			2. make it playable
+			3. deploy
+			4. make it playable with a friend
 			<h1>This is Final Art</h1>
 		</div>
 	)
