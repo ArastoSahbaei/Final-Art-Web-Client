7bc924fe5981e000a33abf876dfe127e769eccbe
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-12-14 16:17:07 +0100

    m

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -113,8 +113,11 @@ export const Board = () => {
 
 	const displayPlayerDeck = () => {
 		return deckOfCards.map((item: card, index: number) =>
-			<Div onMouseOver={() => setCardBeingPlayed(item)} image={item.image} key={index} onMouseDown={e => grabCard(e)}
-				className='ROFLMAO'>
+			<Div className='ROFLMAO'
+				image={item.image}
+				key={index}
+				onMouseOver={() => setCardBeingPlayed(item)}
+				onMouseDown={e => grabCard(e)}>
 				<span>N{item.cardValues.N} </span>
 				<span>E{item.cardValues.E} </span>
 				<span>S{item.cardValues.S} </span>
