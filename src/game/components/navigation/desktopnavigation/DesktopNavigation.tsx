e1571269cddfcf650fa1a9fdf1f92e8abe799c8c
daniel90x <danielpersson90x@gmail.com>
2022-01-13 16:13:59 +0100

    More css and other stuff

--- src/game/components/navigation/desktopnavigation/DesktopNavigation.tsx
+++ src/game/components/navigation/desktopnavigation/DesktopNavigation.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import '../desktopnavigation/DesktopNavigation.css'
 import { useNavigate } from 'react-router-dom'
 
 
@@ -17,3 +17,4 @@ export const NavBar = () => {
 
 
 }
+
