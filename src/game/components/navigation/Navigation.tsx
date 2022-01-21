478c59ddb1cac1018df6e50d89c635d0c75a2afb
daniel90x <danielpersson90x@gmail.com>
2022-01-21 11:03:56 +0100

    removed Constants and changed Nav

--- src/game/components/navigation/Navigation.tsx
+++ src/game/components/navigation/Navigation.tsx
@@ -2,13 +2,13 @@
 import { useWindowDimensions } from 'hooks/useWindowDimensions'
 import { DesktopNavigation } from './desktopnavigation/DesktopNavigation'
 import { MobileNavigation } from './mobilenavigation/MobileNavigation'
-import { MOBILE_NAV_THRESHOLD } from '../../../shared/style/Constants'
 
 export const Navigation = () => {
 	const { width } = useWindowDimensions()
+	const responsiveWidth = 1000
 
 	const determineDimension = () => {
-		return width <= MOBILE_NAV_THRESHOLD
+		return width <= responsiveWidth
 			? <MobileNavigation />
 			: <DesktopNavigation />
 	}
--- src/shared/style/Constants.ts
+++
@@ -1,4 +0,0 @@
-export const DESKTOP_NAV_HEIGHT = 5.2
-export const DESKTOP_NAV_HEIGHT_SMALL = 3.2
-export const MOBILE_NAV_THRESHOLD = 1000
-export const USER_PROFILE_WIDTH = 50
